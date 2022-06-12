4738a005d6e2539ae6d73685ea31cb6d55a94e9a
Akshay Sethi <akshaysethi@Akshays-MacBook-Air.local>
2022-06-12 15:01:06 +0530

    changges to code.py to remove inference from it

--- code.py
+++ code.py
@@ -19,14 +19,3 @@ model.fit(X_train, y_train)
 filename = 'logreg_model.pickle'
 pickle.dump(model, open(filename, 'wb'))
 
-# Model Inference 
-# metric chosen is accuracy since the dataset is well balanced 
-# test trained model on accuracy metric  
-test = pd.read_csv('test.csv')
-X_test = np.array((test.iloc[:, :-1])).reshape(len(test),2)
-y_test = np.array(test.iloc[:, -1:]).reshape(len(test))
-y_pred = model.predict(X_test)
-#print (y_pred.shape, y_test.shape)
-print ("Accuracy on Train Data ", accuracy_score(y_train,model.predict(X_train)))
-print ("Accuracy on Test data ", accuracy_score(y_test,model.predict(X_test)))
-
